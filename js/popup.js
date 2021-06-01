eb20cbdd22c06053c6396bb34888fe46fa93f9a5
Roberto0-0 <84353308+Roberto0-0@users.noreply.github.com>
2021-06-01 11:42:52 -0300

    update0.2

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,14 @@
 const body = document.getElementById('body')
 body.onclick = () => {
     const fullScreen = document.getElementById('full-screen')
-    fullScreen.classList.toggle('pop-up')
-    fullScreen.onclick = (evento) => {
-        console.log(evento.target.id)
-        if(evento.target.className != "remove-pop-up"){
-            fullScreen.classList.remove('pop-up');
-            localStorage.SavePopup = fullScreen
+    if(localStorage.SavePopup != "popup"){
+        fullScreen.classList.toggle('pop-up')
+        fullScreen.onclick = (evento) => {
+            console.log(evento.target.id)
+            if(evento.target.className != "remove-pop-up"){
+                fullScreen.classList.remove('pop-up');
+                localStorage.SavePopup = "popup"
+            }
         }
     }
-    // if(localStorage.SavePopup != fullScreen){
-        
-    // }
 }
